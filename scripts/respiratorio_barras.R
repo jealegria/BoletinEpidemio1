87c0f98b96ed7970baadcf785805c02c97780291
jealegria <juan.ezalegria@gmail.com>
2024-11-27 16:31:51 -0300

    5

--- scripts/respiratorio_barras.R
+++ scripts/respiratorio_barras.R
@@ -15,11 +15,21 @@ conteo_dx <- base_rp_mes_anterior %>%
 
 ##################### Grafico
 
+# Crear la columna Orden_Servicio con valores numéricos según el orden deseado
+conteo_dx$Orden_Servicio <- factor(conteo_dx$Servicio, 
+                                   levels = c("Clinica", "UTIA", "Pediatría", "UTIP", "Neonatología"), 
+                                   labels = c(1, 2, 3, 4, 5))
 
+# Asegurarse de que la columna 'Orden_Servicio' sea un factor ordenado
+conteo_dx$Orden_Servicio <- as.numeric(conteo_dx$Orden_Servicio)
+
+# Crear el gráfico de barras apiladas con los nombres de las variables modificados
 resp_barras1 <- ggplot(conteo_dx, aes(x = Semana, y = Cantidad, fill = Dx)) +
-  geom_bar(stat = "identity", position = "stack") +  # Cambiado a barras apiladas
-  facet_wrap(~ Servicio, nrow = 1, scales = "free_x") +
-  scale_fill_manual(values = c("NR" = "#77FD78", "R" = "#0000FF80", "R - IRAG" = "#FF000080")) +  
+  geom_bar(stat = "identity", position = "stack") +  # Barras apiladas
+  facet_wrap(~ Servicio, nrow = 1, scales = "free_x", 
+             labeller = labeller(Servicio = function(x) factor(x, levels = c("Clinica", "UTIA", "Pediatría", "UTIP", "Neonatología")))) +  # Orden por número
+  scale_fill_manual(values = c("NR" = "#77FD78", "R" = "#0000FF80", "R - IRAG" = "#FF000080"),
+                    labels = c("NR" = "No Respiratoria", "R" = "Respiratoria", "R - IRAG" = "IRAG")) +  # Modificar nombres de las variables
   labs(title = "Distribución de diagnósticos por semanas y servicio (Barras Apiladas)",
        x = "Semana",
        y = "Cantidad",
@@ -30,4 +40,5 @@ resp_barras1 <- ggplot(conteo_dx, aes(x = Semana, y = Cantidad, fill = Dx)) +
         panel.grid = element_blank())  # Elimina las líneas del grid
 
 #resp_barras1
+
 
